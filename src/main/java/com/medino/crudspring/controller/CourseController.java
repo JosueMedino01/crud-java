064ef474dc09e04d0d084fa5e27c536d57ea4126
Josué Medino <josuemedino5@gmail.com>
2024-04-20 12:25:09 -0300

    feat: added findById method

--- src/main/java/com/medino/crudspring/controller/CourseController.java
+++ src/main/java/com/medino/crudspring/controller/CourseController.java
@@ -13,10 +13,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -38,6 +41,14 @@ public class CourseController {
   public List<Course> list(){
     return courseRepository.findAll();
   }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Course> findByIdString(@PathVariable("id") Long id) {
+      return courseRepository.findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build()); 
+  }
+  
 
   @PostMapping
   @ResponseStatus(code = HttpStatus.CREATED)
